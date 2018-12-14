13e88bb093db377f7fca8d3fd4de6f246fd873a6
Sam <sam_hannat@outlook.com>
2018-12-14 15:28:20 -0500

    Added program that gets recipe names and ingredients necessary

--- test.py
+++ test.py
@@ -4,15 +4,36 @@ import time
 driver = webdriver.Chrome()
 driver.get("https://www.food2fork.com/")
 
+
+#Todo: add loop to continually go to the bottom
 """
-for i in range(10):
+for i in range(10)
 	driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-	time.sleep(10)
+
+	time.sleep(5)
 """
+recipes = []
 
-links = [x for x in driver.find_elements_by_class_name("recipe-link")]
-driver.execute_script('''window.open("{}","_blank");'''.format(links[0].get_attribute("href")))
+for link in driver.find_elements_by_class_name("recipe-link")[:3]:
+	
+	driver.execute_script('''window.open("http://www.google.com","_blank");''')
+	url = link.get_attribute("href")
+	tabs = driver.window_handles
+	driver.switch_to.window(tabs[1])
+	driver.get(url)
+	
 
-links[4].send_keys(Keys.CONTROL + "t")
+	recipe_name = driver.find_elements_by_class_name("recipe-title")[0].text
+	ingredients = [] 
 
-print(links)
+	for ingredient in driver.find_elements_by_tag_name("li")[6:]:
+		ingredients.append(ingredient.text)
+
+	recipes.append([recipe_name, ingredients])
+	driver.close()
+	driver.switch_to.window(tabs[0])
+	time.sleep(5)
+
+
+
+print(recipes)
